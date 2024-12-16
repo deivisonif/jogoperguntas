af3b4ec9846a988ef68f3fd62b6dc4dc4b61c608
Ricardo Tenório <126797216+ricardousb@users.noreply.github.com>
2024-12-16 00:56:27 -0300

    Perguntas Múltipla Escolha
    Perguntas com Múltiplas Escolhas

---
+++ PerguntaMultiplaEscolha.java
@@ -0,0 +1,22 @@
+package projeto;
+
+import java.util.List;
+
+public class PerguntaMultiplaEscolha extends Pergunta {
+ private List<String> opcoes;
+
+ public PerguntaMultiplaEscolha(int id, String texto, List<String> opcoes, String respostaCorreta) {
+     super(id, texto, respostaCorreta);
+     this.opcoes = opcoes;
+ }
+
+ @Override
+ public boolean validarResposta(String resposta) {
+     return opcoes.contains(resposta.toUpperCase()) && resposta.equalsIgnoreCase(getRespostaCorreta());
+ }
+
+ public List<String> getOpcoes() {
+     return opcoes;
+ }
+}
+
