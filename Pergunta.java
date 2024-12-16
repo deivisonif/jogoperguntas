85369df35c47bc50ea0bf3341f6dc6610fd007d6
Deivison <126308825+deivisonif@users.noreply.github.com>
2024-12-16 00:43:41 -0300

    Class Pergunta

---
+++ Pergunta.java
@@ -0,0 +1,29 @@
+package projeto;
+
+import java.util.List;
+
+public abstract class Pergunta {
+ private int id;
+ private String texto;
+ private String respostaCorreta;
+
+ public Pergunta(int id, String texto, String respostaCorreta) {
+     this.id = id;
+     this.texto = texto;
+     this.respostaCorreta = respostaCorreta;
+ }
+ 
+ public abstract boolean validarResposta(String resposta);
+
+ public int getId() {
+     return id;
+ }
+
+ public String getTexto() {
+     return texto;
+ }
+
+ public String getRespostaCorreta() {
+     return respostaCorreta;
+ }
+}
