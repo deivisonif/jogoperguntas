cc98348734837d574f2143da19f4e1c986c91244
Rosilene4 <129714743+Rosilene4@users.noreply.github.com>
2024-12-16 00:40:32 -0300

    Perguntas iniciais!
    Adicionei as perguntas iniciais do jogo!

---
+++ PerguntaVF.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class PerguntaVF extends Pergunta {
+
+ public PerguntaVF(int id, String texto, String respostaCorreta) {
+     super(id, texto, respostaCorreta);
+ }
+
+ @Override
+ public boolean validarResposta(String resposta) {
+     return resposta.equalsIgnoreCase(getRespostaCorreta());
+ }
+}
