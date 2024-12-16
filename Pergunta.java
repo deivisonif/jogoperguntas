70403ff4d667bedf2b5e1e43b8f6b333ae6b419a
Nichael Filho <nichaelfilho@hotmail.com>
2024-12-16 08:23:55 -0300

    Update Pergunta.java

--- Pergunta.java
+++ Pergunta.java
@@ -1,29 +1,19 @@
 package projeto;
 
-import java.util.List;
-
 public abstract class Pergunta {
- private int id;
- private String texto;
- private String respostaCorreta;
+    private final int id;
+    private final String texto;
+    private final String respostaCorreta;
 
- public Pergunta(int id, String texto, String respostaCorreta) {
-     this.id = id;
-     this.texto = texto;
-     this.respostaCorreta = respostaCorreta;
- }
- 
- public abstract boolean validarResposta(String resposta);
+    public Pergunta(int id, String texto, String respostaCorreta) {
+        this.id = id;
+        this.texto = texto;
+        this.respostaCorreta = respostaCorreta;
+    }
 
- public int getId() {
-     return id;
- }
+    public abstract boolean validarResposta(String resposta);
 
- public String getTexto() {
-     return texto;
- }
-
- public String getRespostaCorreta() {
-     return respostaCorreta;
- }
+    public int getId() { return id; }
+    public String getTexto() { return texto; }
+    public String getRespostaCorreta() { return respostaCorreta; }
 }
