2e505a01561252ff3efa29fa65ff195c9fcdd0a3
Rosilene4 <129714743+Rosilene4@users.noreply.github.com>
2024-12-19 22:48:17 -0300

    Adicionei a criação do jogador, verificação e o salvamento no banco antes de começar o jogo

--- Main.java
+++ Main.java
@@ -8,12 +8,21 @@ public class Main {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jogo", "root", "");
+            GetNomes getNomes = new GetNomes();
+            Jogador jogador = getNomes.initNames();
+            if (jogador == null) {
+                System.out.println("Erro: Jogador não foi criado corretamente.");
+                return;
+            } else {
+            	System.out.println("Adicionado");
+            }
+            JogadorDAO jogadorDAO = new JogadorDAO(connection);
+            jogadorDAO.inserirJogador(jogador);
             PerguntaDAO perguntaDAO = new PerguntaDAO(connection);
-            JogadorDAO jogadorDAO = new JogadorDAO();
             JogoBO jogoBO = new JogoBO(perguntaDAO, jogadorDAO);
-            JogoService jogoService = new JogoService(jogoBO);
+            JogoService jogoService = new JogoService(connection, jogoBO);
             
-            jogoService.iniciarJogo();
+            jogoService.iniciarJogo(jogador);
         } catch (SQLException e) {
             e.printStackTrace();
         }
