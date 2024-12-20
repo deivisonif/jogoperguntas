44ce113be13daa7c8c3450660d2ef9f141c08123
Deivison <126308825+deivisonif@users.noreply.github.com>
2024-12-20 14:26:03 -0300

    Update Main.java, add verificação e exception de campos nulls

--- Main.java
+++ Main.java
@@ -8,14 +8,28 @@ public class Main {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jogo", "root", "");
+
             GetNomes getNomes = new GetNomes();
-            Jogador jogador = getNomes.initNames();
+            Jogador jogador = null;
+            
+            boolean valid = false;
+            while (!valid) {
+                try {
+                    jogador = getNomes.initNames();
+                    valid = true;
+                } catch (CampoInvalidoException e) {
+                    System.out.println("Erro: " + e.getMessage());
+                    System.out.println("Por favor, insira novamente os dados corretamente.\n");
+                }
+            }
+
             JogadorDAO jogadorDAO = new JogadorDAO(connection);
             jogadorDAO.inserirJogador(jogador);
+
             PerguntaDAO perguntaDAO = new PerguntaDAO(connection);
             JogoBO jogoBO = new JogoBO(perguntaDAO, jogadorDAO);
             JogoService jogoService = new JogoService(connection, jogoBO);
-            
+
             jogoService.iniciarJogo(jogador);
         } catch (SQLException e) {
             e.printStackTrace();
